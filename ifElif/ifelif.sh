835ca454f649d6b1c13a39d932e7c359f89a0bc1
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-12 18:30:24 +0530

    Added:-Added ifelse

---
+++ ifElif/ifelif.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+variable1=20
+variable2=20
+if [ $variable2 -gt $variable1 ]
+then
+   echo "$variable2 is grater than $variable1"
+elif [ $variable2 -eq $variable1 ]
+then
+  echo "variable are eual"
+else
+  echo "$variable2 is smaller then $variable1"
+fi
+
