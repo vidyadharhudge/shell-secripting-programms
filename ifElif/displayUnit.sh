8682e632936357716489e7fd1f060640cd7424d6
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-12 18:26:01 +0530

    Added :- Added display unit

---
+++ ifElif/displayUnit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+read -p "enter a no" x
+
+if [ $x -eq 1 ]
+then
+   echo "unit"
+
+elif [ $x -eq 10 ]
+then
+   echo "ten"
+
+elif [ $x -eq 100 ]
+then
+   echo "hundred"
+
+elif [ $x -eq 1000 ]
+then
+   echo "one thousand"
+
+fi
