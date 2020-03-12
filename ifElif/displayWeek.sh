31c1ec8354bbdae95fd0768948822bb7eba949b0
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-12 18:27:53 +0530

    Added:- added Display week

---
+++ ifElif/displayWeek.sh
@@ -0,0 +1,33 @@
+#i/bin/bash
+read -p "enter a no " x
+
+if [ $x -eq 1 ]
+then
+   echo "sunday"
+elif [ $x -eq 2 ]
+then
+   echo "monday"
+
+elif [ $x -eq 3 ]
+then
+   echo "tuseday"
+
+elif [ $x -eq 4 ]
+then
+   echo "wensday"
+
+elif [ $x -eq 5 ]
+then
+   echo "thursday"
+
+elif [ $x -eq 6 ]
+then
+   echo "friday"
+
+elif [ $x -eq 7 ]
+then
+   echo "satarday"
+
+fi
+
+
