0cd6a455d0d628cb978135b07c6c2f2b58ffee5b
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-12 18:20:18 +0530

    Added:-Added partime

---
+++ ifElif/addingpartime1.sh
@@ -0,0 +1,19 @@
+                            
+#!/bin/blash -x
+ispartime=1;
+isfulltime=2;
+emprateperhour=20;
+randomcheck=$((RANDOM%3));
+
+if [ $isfulltime -eq $randomcheck ];
+then
+    emphrs=8;
+elif [ $ispartime -eq $randomcheck ];
+then
+    emphrs=4;
+else
+  emphrs=0;
+fi
+
+salary=$(($emphrs*$emprateperhour));
+echo $salary
