ab613263d30f4bfa70bbec5aadb845497c50b909
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-13 17:38:19 +0530

    Added:- display unit from no

---
+++ case-selection/displayUnit.sh
@@ -0,0 +1,19 @@
+!/bin/bash -x
+read -p  "enter a number"  number
+case $number in
+
+1)
+  echo unit ;;
+10)
+  echo ten ;;
+100)
+  echo hundred ;;
+1000)
+  echo thousand ;;
+
+100000)
+  echo one lakh ;;
+
+esac
+
+
