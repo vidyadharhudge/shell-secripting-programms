6816e05dfbbe7d3e24109ed1eca602955a4aa700
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-13 17:23:01 +0530

    Added:-read single digit no

---
+++ case-selection/readSingledigitNo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+read -p  "enter a single digit no"  number
+case $number in
+
+0)
+  echo zero ;;
+1)
+  echo one ;;
+2)
+  echo two ;;
+3)
+  echo three ;;
+
+4)
+  echo four ;;
+
+5)
+  echo five ;;
+
+6)
+  echo six ;;
+
+7)
+  echo seven ;;
+
+8)
+  echo eight ;;
+
+9)
+  echo nine ;;
+
+esac
