a7f9305fa48982527174000c789d95be4d0a1ab7
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-13 17:30:09 +0530

    Added:-display week from no

---
+++ case-selection/displayWeek.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+read -p  "enter a number"  number
+case $number in
+
+0)
+  echo sunday ;;
+1)
+  echo monday ;;
+2)
+  echo tuseday ;;
+3)
+  echo wensday ;;
+
+4)
+  echo thursday ;;
+
+5)
+  echo friday ;;
+
+6)
+  echo saturday ;;
+
+esac
+
