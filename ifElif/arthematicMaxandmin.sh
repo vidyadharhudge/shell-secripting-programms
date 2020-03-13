6b65d9f71a735e127249fd31972404c417cfb9cc
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-13 10:27:43 +0530

    Added:-Added max and min value

--- ifElif/arthematicMaxandmin.sh
+++ ifElif/arthematicMaxandmin.sh
@@ -11,18 +11,29 @@ y=$((c+a/b))
 echo $y
 z=$((a*b+c))
 echo $z
-if [ $w -gt $x ] && [ $w -gt $y ] && [ $w -gt $z ]
+if [ $w -gt $x ] && [ $w -gt $y ] && [ $w -gt $z ];
 then
-   echo $w
-elif [ $x -gt $y ] && [ $x -gt $z ] &&  [ $x -gt $w ]
+   echo "w is grater "
+elif [ $x -gt $y ] && [ $x -gt $z ] &&  [ $x -gt $w ];
 then
-   echo $x
-elif [ $y -gt $w ] && [ $y -gt $x ] &&  [ $y -gt $z ]
+   echo  "x is grater"
+elif [ $y -gt $w ] && [ $y -gt $x ] &&  [ $y -gt $z ];
 then
-   echo $y
+   echo "y is grater"
 else 
    echo "z is grater"
-
 fi
 
+if [ $w -lt $x ] && [ $w -lt $y ] && [ $w -lt $z ];
+then
+   echo "w is smaller "
+elif [ $x -lt $y ] && [ $x -lt $z ] &&  [ $x -lt $w ];
+then
+   echo  "x is smaller"
+elif [ $y -lt $w ] && [ $y -lt $x ] &&  [ $y -lt $z ];
+then
+   echo "y is smaller"
+else
+   echo "z is smaller" 
+fi
 
