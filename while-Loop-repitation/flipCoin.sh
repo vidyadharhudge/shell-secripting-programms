161f097c331766a4ef02805c327de94c92472d30
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-14 15:11:09 +0530

    Added:-Head or tail win 11 times

---
+++ while-Loop-repitation/flipCoin.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+H=0;
+T=0;
+count=1;
+while (( $count > 0 ))
+  do
+    toss=$(( RANDOM%2 ))
+       if [ $toss -eq 1 ]
+           then
+             echo $H
+             (( H++ ))
+       else 
+           echo $T
+           (( T++ ))
+           fi 
+
+if [ $H -eq 11 ]
+   then
+     echo "head wins"
+     exit
+elif [ $T -eq 11 ]
+     then
+       echo "tail is win"
+       exit
+       fi
+(( count++ ))
+  done
