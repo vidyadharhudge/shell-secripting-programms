4c877ee946df02ea9745bb23ae544d8febd9b6ea
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-14 13:23:02 +0530

    Added:-printing the table of power 2

---
+++ while-Loop-repitation/printTableofPower2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+read -p "plz enter a number " number
+count=0
+num=1
+while (( $count <= $number )) 
+     do
+       echo $num
+      (( num=2*$num )) 
+      (( count++ ))
+     done
