0af277a077fd69c9d59b2731eea4af04889f9bfd
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-16 12:55:41 +0530

    Added:-two no are palindrome to each other or not

---
+++ function-Programs/palindrome.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+function checkPalindrome()
+{
+ sum=0 
+ temp=0
+ temp=$number
+   while (( $number != 0 ))
+    do
+     reminder=$number%10
+     (( sum=$sum*10+$reminder ))
+     number=$number/10
+    done
+if [ $temp -eq $sum ]
+   then
+      echo $sum
+else
+     echo "number is not palindrome"
+     fi
+}
+
+read -p "enter a no to palindrome" number
+res1="$( checkPalindrome $(($number)) )"
+read -p "enter a no to palindrome" number
+res2="$( checkPalindrome $(($number)) )" 
+
+if [ $res1 -eq $res2 ]
+then
+   echo "two are palindrome to each other"
+else
+  echo "two no are not palindrome to each other"
+fi
