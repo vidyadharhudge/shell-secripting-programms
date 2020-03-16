3378109842739fe09030b84c14a84d087391b14b
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-16 17:45:52 +0530

    Added:-To find prime factor

--- for-loop-repitation/primeFactorization.sh
+++ for-loop-repitation/primeFactorization.sh
@@ -1,28 +1,15 @@
 #!/bin/bash 
-read -p "enter the number to be prime " number
-for (( i=2; i<number; i++ )) #less than no bcz the no which is to fact not req
+read -p "enter the number" number
+count=0
+for (( i=2; i<=$number; i++ ))
   do
-     if ((  number%i==0 ))
-     then 
-         count=0
-           for ((  j=1; j<=i; j++ ))
-            do
-               if (( i%j==0 ))
-                then
-                     (( count++ ))
-                fi
-            done
-            if [ $count==2 ]
-            then
-                  printf "\n$i"
-            fi
-fi
+    while (( $number%$i==0 ))
+      do
+           echo $i
+           number=$number/$i
 
   done
-
-     printf "\n"
-
-    
+  done
 
 
 
