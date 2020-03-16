25260e65b6f2276d445bccb339fef9c723079f4e
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-16 13:01:49 +0530

    Added:-gambling game to find no of win and bet

---
+++ while-Loop-repitation/gambler.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+read -p "enter a wallet amount :" walletAmount
+read -p "enter a goal amount " goal
+counter=0;
+i=0;
+while true
+   do
+    bet=$((RANDOM%2));
+  if [ $bet -eq 0 ]
+     then
+       walletAmount=$(($walletAmount-1));
+       ((i++));
+  else
+    walletAmount=$(($walletAmount+$bet));
+    ((counter++));
+    ((i++));
+ fi
+if [ $walletAmount -le 0 ]
+  then
+     echo "gambler has lost"
+     break;
+elif [ $goal -lt $walletAmount ]
+  then
+   echo "gambler has won, $counter time wins and amount is $walletAmount"
+   break;
+fi
+done
+
