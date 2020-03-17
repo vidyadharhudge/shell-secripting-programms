f38a178b5e74090943495a343943976f1c5f1209
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-17 17:08:25 +0530

    Added:-Min and max

---
+++ dictionary-programms/minMax.sh
@@ -0,0 +1,36 @@
+#!/bin/bash  
+declare -A res
+diceno=1
+while [[ res[$diceno] -lt 10 ]]
+do
+diceno=$(( (RANDOM%6)+1))
+res[$diceno]=$(( ${res[$diceno]}+1 ))
+done
+if [[ res[$diceno] -eq 10 ]]
+then
+  echo "the no that reached max time is: $diceno"
+fi
+min=${res[1]}
+for (( i=1; i<=6; i++ ))
+ do
+if [ ${res[$i]} -le $min  ]
+then
+   min=${res[$i]}
+   fi
+done
+   echo $min
+
+for (( i=1; i<=6; i++ ))
+do
+if [[ $min -eq ${res[$i]} ]]
+then
+echo "min index $i"
+fi
+done
+
+
+
+echo ${!res[@]}
+echo ${res[@]}
+
+
