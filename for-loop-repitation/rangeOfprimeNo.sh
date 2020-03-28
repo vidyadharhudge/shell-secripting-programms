92bdfe96d0174157d734b7f8ca5f001a858752e1
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 12:02:22 +0530

    Added:-TO Find Prime No Within Range

--- for-loop-repitation/rangeOfprimeNo.sh
+++ for-loop-repitation/rangeOfprimeNo.sh
@@ -2,21 +2,21 @@
 read -p "enter the startnig range " number
 read -p "enter the ending range " number1
 for (( i=$number; i<=$number1; i++ ))
-    do
-         count=0
-         for (( j=1; j<=$i; j++ ))
-           do
-             if (( $i%$j==0 ))
-                then
-                   (( count++ ))
-                fi
-          done
-              if [ $count == 2 ]
-                 then
-                   echo $i
+do
+    count=0
+for (( j=1; j<=$i; j++ ))
+do
+    if (( $i%$j==0 ))
+    then
+         (( count++ ))
+    fi
+done
+    if [ $count == 2 ]
+    then
+         echo $i
 
-              fi
+    fi
 
-    done       
+ done       
 
 
