dcf47600cd2cccd35c7c268da588cefdbbdb685f
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 12:39:01 +0530

    Added:-Gambling Programme

--- while-Loop-repitation/gambler.sh
+++ while-Loop-repitation/gambler.sh
@@ -4,25 +4,25 @@ read -p "enter a goal amount " goal
 counter=0;
 i=0;
 while true
-   do
+do
     bet=$((RANDOM%2));
-  if [ $bet -eq 0 ]
-     then
-       walletAmount=$(($walletAmount-1));
-       ((i++));
-  else
-    walletAmount=$(($walletAmount+$bet));
-    ((counter++));
-    ((i++));
- fi
-if [ $walletAmount -le 0 ]
-  then
-     echo "gambler has lost"
-     break;
-elif [ $goal -lt $walletAmount ]
-  then
-   echo "gambler has won, $counter time wins and amount is $walletAmount"
-   break;
-fi
+    if [ $bet -eq 0 ]
+    then
+         walletAmount=$(($walletAmount-1));
+         ((i++));
+    else
+         walletAmount=$(($walletAmount+$bet));
+        ((counter++));
+        ((i++));
+    fi
+    if [ $walletAmount -le 0 ]
+    then
+         echo "gambler has lost"
+         break;
+    elif [ $goal -lt $walletAmount ]
+    then
+         echo "gambler has won, $counter time wins and amount is $walletAmount"
+    break;
+    fi
 done
 
