26bbf520a73bd9b2144461b449f4d57fccd94e68
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 12:15:55 +0530

    Added:-To Find Display Unit

--- ifElif/displayUnit.sh
+++ ifElif/displayUnit.sh
@@ -3,18 +3,18 @@ read -p "enter a no" x
 
 if [ $x -eq 1 ]
 then
-   echo "unit"
+      echo "unit"
 
 elif [ $x -eq 10 ]
 then
-   echo "ten"
+      echo "ten"
 
 elif [ $x -eq 100 ]
 then
-   echo "hundred"
+      echo "hundred"
 
 elif [ $x -eq 1000 ]
 then
-   echo "one thousand"
+      echo "one thousand"
 
 fi
