50f226cc9eea002f379982bf0e705926b70f36e4
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 11:36:55 +0530

    Added:-Finding Prime Factorization

--- array-Programms/primeFactorization.sh
+++ array-Programms/primeFactorization.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -x 
 function checkprimefactor()
 {
-count=0
-for (( i=2; i<=$number; i++ ))
+  count=0
+  for (( i=2; i<=$number; i++ ))
   do
-    while (( $number%i==0 ))
+      while (( $number%i==0 ))
       do
            echo $i
            array[((count++))]=$i
            number=$number/$i
            
-  done
-  done
+     done
+ done
 }
 
 read -p "enter the number" number
