0020d9ee7588fd6d4096a895d5b9893b93a973b6
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 11:50:17 +0530

    Added:-To Compute The Factorial No

--- for-loop-repitation/computesFactorialno.sh
+++ for-loop-repitation/computesFactorialno.sh
@@ -2,7 +2,7 @@
 read -p "enter a number " number
 fact=1
 for (( i=1; i<=$number; i++ ))
-      do
-        ((fact=$fact*$i))
-      done
-           echo $fact
+do
+    ((fact=$fact*$i))
+done
+    echo $fact
