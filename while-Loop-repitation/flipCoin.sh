621bc8ab053cba47ec0edaf44b419631812c6b12
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 12:36:37 +0530

    Added:-Flip The Coin

--- while-Loop-repitation/flipCoin.sh
+++ while-Loop-repitation/flipCoin.sh
@@ -3,25 +3,25 @@ H=0;
 T=0;
 count=1;
 while (( $count > 0 ))
-  do
+do
     toss=$(( RANDOM%2 ))
-       if [ $toss -eq 1 ]
-           then
-             echo $H
-             (( H++ ))
-       else 
-           echo $T
-           (( T++ ))
-           fi 
+    if [ $toss -eq 1 ]
+    then
+         echo $H
+         (( H++ ))
+    else 
+         echo $T
+         (( T++ ))
+         fi 
 
-if [ $H -eq 11 ]
-   then
-     echo "head wins"
-     exit
-elif [ $T -eq 11 ]
-     then
-       echo "tail is win"
-       exit
-       fi
-(( count++ ))
-  done
+    if [ $H -eq 11 ]
+    then
+         echo "head wins"
+    exit
+    elif [ $T -eq 11 ]
+    then
+         echo "tail is win"
+    exit
+    fi
+    (( count++ ))
+done
