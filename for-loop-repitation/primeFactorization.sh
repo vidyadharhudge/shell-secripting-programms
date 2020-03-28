75d4fc55a9beb75d27094e30f5bfd726dd756665
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 11:56:06 +0530

    Added:-To Find Prime Factorization

--- for-loop-repitation/primeFactorization.sh
+++ for-loop-repitation/primeFactorization.sh
@@ -1,15 +1,15 @@
-#!/bin/bash 
+#!/bin/bash -x 
 read -p "enter the number" number
 count=0
 for (( i=2; i<=$number; i++ ))
-  do
+do
     while (( $number%$i==0 ))
-      do
+    do
            echo $i
            number=$number/$i
 
   done
-  done
+done
 
 
 
