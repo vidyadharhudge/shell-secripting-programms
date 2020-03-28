46a65b9b0fe80a5600287f2abbdfe4ce3c9ce773
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 11:59:38 +0530

    Added:-To Find Prime No

--- for-loop-repitation/primeNo.sh
+++ for-loop-repitation/primeNo.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
 read -p "enter the number to be prime " number
 count=0
- for (( j=1; j<=$number; j++ ))
-        do
-           if (( $number%$j==0 ))
-             then 
-               (( count++ ))
-        fi
-        done
+for (( j=1; j<=$number; j++ ))
+do
+    if (( $number%$j==0 ))
+    then 
+          (( count++ ))
+    fi
+done
 if (( $count == 2 ))
-   then
+then
       echo "no is prime"
 else
-     echo "no is not prime"
+      echo "no is not prime"
 fi
 
 
