d3b58906068ce06cdd437d032afdb698e8aacf13
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 12:07:23 +0530

    Added:-To Find Palindrome No

--- function-Programs/palindrome.sh
+++ function-Programs/palindrome.sh
@@ -4,28 +4,27 @@ function checkPalindrome()
  sum=0 
  temp=0
  temp=$number
-   while (( $number != 0 ))
-    do
+ while (( $number != 0 ))
+ do
      reminder=$number%10
      (( sum=$sum*10+$reminder ))
      number=$number/10
-    done
-if [ $temp -eq $sum ]
-   then
+ done
+ if [ $temp -eq $sum ]
+ then
       echo $sum
-else
-     echo "number is not palindrome"
-     fi
+ else
+      echo "number is not palindrome"
+ fi
 }
 
 read -p "enter a no to palindrome" number
 res1="$( checkPalindrome $(($number)) )"
 read -p "enter a no to palindrome" number
 res2="$( checkPalindrome $(($number)) )" 
-
 if [ $res1 -eq $res2 ]
 then
-   echo "two are palindrome to each other"
+     echo "two are palindrome to each other"
 else
-  echo "two no are not palindrome to each other"
+     echo "two no are not palindrome to each other"
 fi
