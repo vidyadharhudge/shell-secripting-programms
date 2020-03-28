d87fea5471c37750a45e464aead51424d082b54a
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 12:17:47 +0530

    Added:-To Showing Ifelif Statment

--- ifElif/ifelif.sh
+++ ifElif/ifelif.sh
@@ -3,11 +3,11 @@ variable1=20
 variable2=20
 if [ $variable2 -gt $variable1 ]
 then
-   echo "$variable2 is grater than $variable1"
+     echo "$variable2 is grater than $variable1"
 elif [ $variable2 -eq $variable1 ]
 then
-  echo "variable are eual"
+     echo "variable are eual"
 else
-  echo "$variable2 is smaller then $variable1"
+     echo "$variable2 is smaller then $variable1"
 fi
 
