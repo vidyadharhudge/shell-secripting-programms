c2539ac7d3145f27d93f57f4aeccf95e1f29a93e
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 11:34:59 +0530

    Added:-Finding The Digit That Repeated

--- array-Programms/findThedigitrepeated.sh
+++ array-Programms/findThedigitrepeated.sh
@@ -2,10 +2,10 @@
 count=0
 for (( i=1; i<=100; i++ ))
 do
-rem=$(($i%11))
-  if [ $rem -eq 0 ]
-  then
-     array[((count++))]=$i
-fi
+   rem=$(($i%11))
+   if [ $rem -eq 0 ]
+   then
+        array[((count++))]=$i
+   fi
 done 
- echo ${array[@]}
+echo ${array[@]}
