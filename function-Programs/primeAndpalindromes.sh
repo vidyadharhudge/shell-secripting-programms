be88c43ff989b21dad31e23652e10c46cb4e8c43
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 12:09:55 +0530

    Added:-To Find Prime Palindrome

--- function-Programs/primeAndpalindromes.sh
+++ function-Programs/primeAndpalindromes.sh
@@ -2,39 +2,40 @@
 
 function checkPrime()
 {
-count=0
+  count=0
   for (( i=1; i<=$number; i++ ))
-   do
-    if (( $number%i == 0 ))
+  do
+     if (( $number%i == 0 ))
      then
-       (( count++ ))
-    fi
-   done
+         (( count++ ))
+     fi
+  done
   if [ $count == 2 ]
-    then
-     "$(checkPalindrome $(($number)) )"
+  then
+         "$(checkPalindrome $(($number)) )"
   else
-    echo $ "no is not prime"
-    fi
+          echo $ "no is not prime"
+  fi
 }
+
 function checkPalindrome()
 { 
- sum=0
- temp=0
- temp=$number
+   sum=0
+   temp=0
+   temp=$number
    while (( $number != 0 ))
-    do
-     reminder=$number%10
-     (( sum=$sum*10+$reminder ))
-     number=$number/10
-    done
+   do
+        reminder=$number%10
+        (( sum=$sum*10+$reminder ))
+        number=$number/10
+   done
 if [ $temp -eq $sum ]
-   then
+then
       echo "number is palindrome and prime"
 else
-     echo "number is not palindrome"
+      echo "number is not palindrome"
 fi
 }
-     read -p "enter a number " number
-     "$(checkPrime $(($number)) )"
+read -p "enter a number " number
+$(checkPrime $(($number)) )"
 
