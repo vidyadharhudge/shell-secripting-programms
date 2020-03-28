2d257f4dc27759ee105425abd49a626b9da524c2
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-28 12:28:29 +0530

    Added:-Year Is Leap Year Or Not

--- selection-statment-if-else/leapYear.sh
+++ selection-statment-if-else/leapYear.sh
@@ -2,10 +2,10 @@
 read -p "enter a year" year
 if [ $(($year%4)) -eq 0 ] && [ $(($year%100)) -ne 0 ] 
 then
-echo "the year is leap year"
+     echo "the year is leap year"
 elif [ $(($year%400)) -eq 0 ]
 then
-echo "the year is leap year"
+     echo "the year is leap year"
 else
-echo "the year is not leap year"
+     echo "the year is not leap year"
 fi
